765ac9d76f86d70dacd6b43cb826d42e8e1c837e
hendraw <hendra.wijaya@bizzy.co.id>
2019-07-13 23:16:44 +0700

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       \App\User::insert([
+            [
+              'id'  			=> 1,
+              'name'  			=> 'Admin',
+              'username'		=> 'admin',
+              'email' 			=> 'admin@cariprogram.id',
+              'password'		=> bcrypt('admin'),
+              'gambar'			=> NULL,
+              'level'			=> 'admin',
+              'remember_token'	=> NULL,
+              'created_at'      => \Carbon\Carbon::now(),
+              'updated_at'      => \Carbon\Carbon::now()
+            ],
+            [
+              'id'  			=> 2,
+              'name'  			=> 'User',
+              'username'		=> 'user',
+              'email' 			=> 'user@cariprogram.id',
+              'password'		=> bcrypt('user'),
+              'gambar'			=> NULL,
+              'level'			=> 'user',
+              'remember_token'	=> NULL,
+              'created_at'      => \Carbon\Carbon::now(),
+              'updated_at'      => \Carbon\Carbon::now()
+            ]
+        ]);
+    }
+}
